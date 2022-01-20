f51031cae887120acceb91a494a6731ef275bc6b
Atm0s <atm0s@ya.ru>
2022-01-20 19:53:54 +0300

    feat: implement the human readable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (num) {
+  const number = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const moreNumbers = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const decimals = ['', 'ten' ,'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let result = '';
+ 
+  if (num.toString().length === 3) {
+    let hundreds = Math.floor(num * 0.01);
+    num -= hundreds * 100;
+    result += `${number[hundreds]} hundred `;
+  }
+
+  if (num.toString().length === 2 && (num >= 20 || num == 10)) {
+    let dec = Math.floor(num * 0.1);
+    num -= dec * 10;
+    result += `${decimals[dec]} `;
+  }
+
+  else if (num.toString().length === 2 && num < 20) {
+    result += `${moreNumbers[num - 10]} `;
+    num = '';
+  }  
+
+  if (num.toString().length === 1) {
+    result += `${number[num]}`;
+  }
+
+  if (result == '' && num == '0') return 'zero'
+  return (result.slice(-1) == " ") ? result.slice(0, -1) : result
 }
